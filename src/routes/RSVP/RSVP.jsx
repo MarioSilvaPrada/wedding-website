b21aa633350416afb7c1224027c247acdaaaa5b2
Mário Prada <mariosilvaprada@gmail.com>
2020-05-09 00:11:04 +0100

    adding form

--- src/routes/RSVP/RSVP.jsx
+++ src/routes/RSVP/RSVP.jsx
@@ -1,8 +1,113 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import * as S from './RSVP.styled';
 
-const RSVP = () => (
-  <S.StyledContainer />
-);
+const RSVP = () => {
+  const [email, setEmail] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [confirmation, setConfirmation] = useState('');
+  const [plus, setPlus] = useState('');
+
+  // const [isSubmit, setIsSubmitted] = useState(false);
+
+  // const onSubmit = () => {
+  //   setIsSubmitted(true);
+  // };
+
+  // const formInfo = [
+  //   {
+  //     label: 'E-mail *',
+  //     type: 'email',
+  //     name: 'entry.1144025166',
+  //     value: email,
+  //     onChange: (e) => setEmail(e.target.value),
+  //   },
+  //   {
+  //     label: 'Primeiro nome *',
+  //     type: 'text',
+  //     name: 'entry.1144025166',
+  //     value: firstName,
+  //     onChange: (e) => setFirstName(e.target.value),
+  //   },
+  //   {
+  //     label: 'Último nome *',
+  //     type: 'text',
+  //     name: 'entry.684504895',
+  //     value: lastName,
+  //     onChange: (e) => setLastName(e.target.value),
+  //   },
+  //   {
+  //     label: 'Confirmas a tua presença? *',
+  //     type: 'text',
+  //     name: 'entry.851837353',
+  //     value: confirmation,
+  //     onChange: (e) => setConfirmation(e.target.value),
+  //   },
+  //   {
+  //     label: 'Quem mais vem contigo? *',
+  //     type: 'text',
+  //     name: 'entry.2028181525',
+  //     value: plus,
+  //     onChange: (e) => setPlus(e.target.value),
+  //   },
+  // ];
+
+  return (
+    <S.StyledContainer>
+      <S.StyledImage />
+      <S.StyledForm
+        method="POST"
+        action="https://docs.google.com/forms/u/0/d/e/1FAIpQLScl0AqV1WgzQ-hERFRqXZVdGj_YW7CxjcnxpVV0LzEccZaNpQ/formResponse"
+        target="hidden_iframe"
+      >
+        <S.StyledInputContainer>
+          <S.Label>E-mail *</S.Label>
+          <S.StyledInput
+            type="email"
+            name="entry.1144025166"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </S.StyledInputContainer>
+        <S.InputWrapper>
+          <S.StyledInputContainer>
+            <S.Label>Primeiro nome *</S.Label>
+            <S.StyledInput
+              name="entry.2087119626"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </S.StyledInputContainer>
+          <S.StyledInputContainer>
+            <S.Label>Último nome *</S.Label>
+            <S.StyledInput
+              name="entry.684504895"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </S.StyledInputContainer>
+        </S.InputWrapper>
+        <S.StyledInputContainer>
+          <S.Label>Confirmas a tua presença? *</S.Label>
+          <S.StyledInput
+            name="entry.851837353"
+            value={confirmation}
+            onChange={(e) => setConfirmation(e.target.value)}
+          />
+        </S.StyledInputContainer>
+        <S.StyledInputContainer>
+          <S.Label>Quem mais vem contigo? *</S.Label>
+          <S.StyledInput
+            name="entry.2028181525"
+            value={plus}
+            onChange={(e) => setPlus(e.target.value)}
+          />
+        </S.StyledInputContainer>
+        <S.SubmitButton type="submit" value="Enviar" />
+      </S.StyledForm>
+    </S.StyledContainer>
+  );
+};
 
 export default RSVP;
--- src/routes/RSVP/RSVP.styled.jsx
+++ src/routes/RSVP/RSVP.styled.jsx
@@ -2,7 +2,64 @@ import styled from 'styled-components';
 
 export const StyledContainer = styled.div`
   background: ${(props) => props.theme.colors.lightGreen};
-  height: 50vh;
+  min-height: 100vh;
 `;
 
 export const Wrapper = styled.div`background: ${(props) => props.theme.colors.lightGreen};`;
+
+export const StyledForm = styled.form`
+  width: 33rem;
+  padding: 1rem 2rem;
+  margin: 0 auto;
+  padding: 3rem 0;
+`;
+
+export const Label = styled.label`
+  font-size: 1.1rem;
+  margin-bottom: .5rem;
+  display: block;
+  font-weight: 300;
+`;
+
+export const StyledInput = styled.input`
+  width: 100%;
+  border: none;
+  padding: .6rem;
+  font-size: 1.1rem;
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: 300;
+  border-radius: 4px;
+  border: 1px solid black;
+`;
+
+export const StyledInputContainer = styled.div`
+  width: 100%;
+  margin-bottom: 1.5rem;
+
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+
+  & > :first-child {
+    margin-right: 1rem;
+  }
+`;
+
+export const SubmitButton = styled.input`
+  padding: .9rem 2rem;
+  border-radius: 2rem;
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 1rem;
+  border: 1px solid black;
+  cursor: pointer;
+  font-weight: 800;
+  margin-top: 1rem;
+`;
+
+export const StyledImage = styled.div`
+  background: url('https://images.pexels.com/photos/173665/pexels-photo-173665.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+  height: 50vh;
+  width: 100%;
+  background-size: cover;
+`;
